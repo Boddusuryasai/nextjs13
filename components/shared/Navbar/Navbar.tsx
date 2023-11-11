735438d865614f7185382d97393ecf8e8f95d4ba
suryasai <suryasaichanti@gmail.com>
2023-11-11 13:36:40 +0530

    mobile navbar and search component

--- components/shared/Navbar/Navbar.tsx
+++ components/shared/Navbar/Navbar.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Theme from "./Theme";
+import GlobalSearch from "../search/GlobalSearch";
+import MobileNav from "./MobileNav";
 
 
 const Navbar = () => {
@@ -28,7 +30,7 @@ const Navbar = () => {
           Dev <span className="text-primary-500">Overflow</span>
         </p>
       </Link>
-      Globalsearch
+     <GlobalSearch/>
       <div className="flex-between gap-5">
         <Theme/>
         <SignedIn>
@@ -44,7 +46,7 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
-        mobilenav
+        <MobileNav/>
       </div>
     </nav>
   );
